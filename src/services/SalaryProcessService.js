c28ead3db25d0593cc56048a37fb87f76d2acae2
Isuru Bandara <isurubandara318@gmail.com>
2025-07-28 16:58:00 +0530

    allownces update

--- src/services/SalaryProcessService.js
+++ src/services/SalaryProcessService.js
@@ -1,47 +1,23 @@
 import axios from "@utils/axios";
 
-const getSalaryData = async (month, year, company_id, department_id) => {
+export const getSalaryData = async (month, year, company_id, department_id) => {
   try {
     const response = await axios.get(
       `/salary/process/employees-by-month?month=${month}&year=${year}&company_id=${company_id}&department_id=${department_id}`
     );
-    // const response = await axios.get(`/salary/process/employees-by-month?month=8&year=2025&company_id=3&department_id=7`);
-    return response.data.data;
+    return response.data;
   } catch (error) {
-    console.error("Error fetching rosters:", error);
+    console.error("Error fetching salary data:", error);
     throw error;
   }
 };
 
-// const createRoster = async (rosterData) => {
-//   try {
-//     const response = await axios.post(`/rosters`, rosterData);
-//     return response.data.data;
-//   } catch (error) {
-//     console.error("Error creating roster:", error);
-//     throw error.response?.data?.errors || error.message;
-//   }
-// };
-
-// const updateRoster = async (id, rosterData) => {
-//   try {
-//     const response = await axios.put(`/rosters/${id}`, rosterData);
-//     return response.data.data;
-//   } catch (error) {
-//     console.error("Error updating roster:", error);
-//     throw error.response?.data?.errors || error.message;
-//   }
-// };
-
-// const deleteRoster = async (id) => {
-//   try {
-//     await axios.delete(`/rosters/${id}`);
-//   } catch (error) {
-//     console.error("Error deleting roster:", error);
-//     throw error;
-//   }
-// };
-
-export default {
-  getSalaryData,
+export const UpdateAllowances = async (allowances) => {
+  try {
+    const response = await axios.post(`/salary/process/allowances`, allowances);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching salary data:", error);
+    throw error;
+  }
 };
